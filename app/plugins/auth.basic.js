23058994810018d8fde3b3d3f006d4709268c295
Glitch (scalloped-dune-wasabi) <none>
2021-07-25 11:19:48 +0000

    🐘🕌 Updated with Glitch

--- app/plugins/auth.basic.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = () => {
-  const auth = {
-    'basic':(user, passwd, content) => {
-      return content.includes(`${user}:${passwd}`)
-    }
-  }
-  return { name:'BasicAuth' , version:'1.0' , auth }
-}
--- update.sh
+++ update.sh
@@ -1,39 +1,71 @@
-#!/bin/bash
+#!bash
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+if [ $# -eq 0 ]; then
+  echo "
+"$0" -i|u [-b branch]
+        i       a new install with empty config.
+        u       update use exist config.
+        b       install the branch after parameter b, default master
 
-echo "+============================================================+"
-echo "|                    ShareList Installer                     |"
-echo "|                                                            |"
-echo "|                                         <reruin@gmail.com> |"
-echo "|------------------------------------------------------------|"
-echo "|                                         https://reruin.net |"
-echo "+============================================================+"
-echo ""
-
-echo -e "\n|  ShareList is updating ... "
-
-# deps
-if [ -n "$(command -v apt-get)" ]
-then
-  apt-get install -y wget unzip >/dev/null 2>&1
-elif [ -n "$(command -v yum)" ]
-then
-  yum install -y wget unzip >/dev/null 2>&1
+example:
+        "$0" -i
+        "$0" -u
+        "$0" -b master
+        "$0" -u -b master
+        "$0" -ib test
+"
+#  exit
+  echo "###############
+0, new install
+1, update"
+  read -p "Input:" c
+  [ g"$c" == g"0" ] && install=1
+  [ g"$c" == g"1" ] && update=1
 fi
 
+i=0
+para[$i]=$0
+for av in "$@"
+do
+#echo $av
+  ((i++))
+  para[$i]=$av
+  if [ g"${av:0:1}" == g"-" ]; then
+    while [ g"$av" != g"" ]
+    do
+      ag=${av:0:1}
+      av=${av:1}
+      [ g"$ag" == g"b" ] && isbranch=1
+      [ g"$ag" == g"i" ] && install=1
+      [ g"$ag" == g"u" ] && update=1
+    done
+  else
+    if [ g"$isbranch" == g"1" ]; then
+      branch="-b $av"
+      isbranch=0
+    fi
+  fi
+done
 
-echo -e "|\n|  Download ShareList Package ... "
-wget -O sharelist-master.zip https://github.com/reruin/sharelist/archive/master.zip >/dev/null 2>&1
+if [ g"$install" == g"1" -a g"$update" == g"1" ]; then
+  echo "Both install & update, exit"
+  exit
+fi
+if [ g"$install" != g"1" -a g"$update" != g"1" ]; then
+  echo "Not install & Not update, exit"
+  exit
+fi
 
-unzip -q -o sharelist-master.zip -d ./
-rm -rf ./plugins
-rm -rf ./app/plugins
-cp -rf sharelist-master/* ./
-rm -rf sharelist-master*
+gitsource='https://github.com/qkqpttgf/OneManager-php'
 
-echo -e "|\n|  Install Dependents ... "
-npm install >/dev/null 2>&1
-pm2 restart all >/dev/null 2>&1
+OneManagerPath=`cd $(dirname $0);pwd -P`
+cd ${OneManagerPath}
 
-echo -e "|\n|  Success: ShareList has been updated\n"
+git clone ${branch} ${gitsource}
+[ g"$install" == g"1" ] || \mv -b .data/config.php OneManager-php/.data/
+\mv -b OneManager-php/* ./
+\mv -b OneManager-php/.[^.]* ./
+rm -rf *~
+#rm -rf .[^.]*~
+rm -rf OneManager-php
+chmod 666 .data/config.php
